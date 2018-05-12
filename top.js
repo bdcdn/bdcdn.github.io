c0801350f6a3f2703ecf2c23d74658dcdff8b55f
bdcdn <39188062+bdcdn@users.noreply.github.com>
2018-05-12 13:43:57 +0800

    Update top.js

--- top.js
+++ top.js
@@ -1,5 +1,5 @@
 document.writeln("<div align=\'center\' style=\'margin:5px 0 5px 20px;\'>");
-document.writeln("<a href=\'https://349603.cc/sese/153dy.html\' target=\'_blank\'><img name=\'ad\' src=\'//i.loli.net/2018/05/11/5af53f8d19742.gif\' width=\'100%\' height=\'70\' /></a>");
+document.writeln("<a href=\'https://349603.cc/sese/153dy.html\' target=\'_blank\'><img name=\'ad\' src=\'//ae01.alicdn.com/kf/HTB1azMKfDmWBKNjSZFB5jXxUFXaL.gif\' width=\'100%\' height=\'70\' /></a>");
 document.writeln("<A href=\'http://www.9279927.com:88/376845.html\' target=\'_blank\'><img border=\'0\' src=\'//ae01.alicdn.com/kf/HTB1Lw4DfsIrBKNjSZK95jagoVXaQ.gif\' width=\'100%\' height=\'60\'></A>");
 document.writeln("<A href=\'http://www.8014445.com:88/13072.html\' target=\'_blank\'><img border=\'0\' src=\'//ae01.alicdn.com/kf/HTB1vqrEfBjTBKNjSZFu5jb0HFXaP.gif\' width=\'100%\' height=\'60\'></A>");
 document.writeln("</div>");
